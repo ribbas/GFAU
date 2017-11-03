26d35ed009a286ea4c8767d4f55f8cd799e27f12
josazuwa <osa2@umbc.edu>
2017-11-03 12:15:27 -0400

    Add files via upload

---
+++ vhdl/and3.vhd
@@ -0,0 +1,22 @@
+--and3.vhd
+
+library STD;
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity and3 is
+
+port (
+    in1     :   in  std_logic;
+    in2     :   in  std_logic;
+	 in3     :   in  std_logic;
+    out1    :   out std_logic);
+end and3;
+
+architecture structural of and3 is
+
+begin
+
+    out1 <= in1 and in2 and in3;
+
+end structural;
