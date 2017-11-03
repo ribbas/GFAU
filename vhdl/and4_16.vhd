9ac50529198a87f8db0d9289e5f92b9fe4f41e4b
josazuwa <osa2@umbc.edu>
2017-11-03 11:14:41 -0400

    Update and4_16.vhd

--- vhdl/and4_16.vhd
+++ vhdl/and4_16.vhd
@@ -7,17 +7,15 @@ entity and4_16 is
 port (
     in1     :   in  std_logic;
     in2     :   in  std_logic;
-	 in3     :   in  std_logic;
-	 in4     :   in  std_logic_vector(15 downto 0);
+    in3     :   in  std_logic;
+    in4     :   in  std_logic_vector(15 downto 0);
     out1    :   out std_logic_vector(15 downto 0));
 end and4_16;
 
 architecture structural of and4_16 is
 
 begin
-
-	BIT_AND  :   for i in 15 downto 0 generate
-		out1(i) <= in1 and in2 and in3 and in4(i);
-	end generate BIT_AND;
-
+    BIT_AND  :   for i in 15 downto 0 generate
+        out1(i) <= in1 and in2 and in3 and in4(i);
+    end generate BIT_AND;
 end structural;
