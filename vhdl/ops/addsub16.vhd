c2e1ac8554e3d75893f5115e3d3a37416a5c2ff0
josazuwa <osa2@umbc.edu>
2017-11-03 12:26:52 -0400

    Update addsub16.vhd

--- vhdl/ops/addsub16.vhd
+++ vhdl/ops/addsub16.vhd
@@ -1,38 +1,12 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    10:44:58 10/23/2017 
--- Design Name: 
--- Module Name:    addSub - Structural 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
-----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity addSub is
-    Port ( op1 : in  STD_LOGIC_VECTOR (15 downto 0);
-           op2 : in  STD_LOGIC_VECTOR (15 downto 0);
-           output : out  STD_LOGIC_VECTOR (15 downto 0));
+   
+port ( 
+    op1 : in  STD_LOGIC_VECTOR (15 downto 0);
+    op2 : in  STD_LOGIC_VECTOR (15 downto 0);
+    output : out  STD_LOGIC_VECTOR (15 downto 0));
 end addSub;
 
 architecture structural of addSub is
@@ -50,5 +24,5 @@ COMPUTE  :   for i in 15 downto 0 generate
     XOR_X  :   xor2 port map (op1(i), op2(i), output(i));
 end generate COMPUTE;
 
-end Structural;
+end structural;
 
