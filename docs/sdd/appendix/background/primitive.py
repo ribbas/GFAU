ff9fc8001b9e8e6845d8d27e9dc0d71782d2c5d6
sabbirahm3d <sabbir0ahmed0@gmail.com>
2018-01-06 17:28:24 -0500

    add full list of primitive polynomials

--- docs/sdd/appendix/background/primitive.py
+++ docs/sdd/appendix/background/primitive.py
@@ -6,13 +6,12 @@ if __name__ == "__main__":
     max_deg = 15
     with open("primitive_polynomials_GF2.txt") as poly_file:
         for line in poly_file:
-            bit_vect = ["0"] * max_deg + ["1"]
-            polynomial = line.replace("\n", "")[
-                :-4].replace(" + ", "").split("x^")[1:]
+            bit_vect = ["1"] + ["0"] * max_deg
+            polynomial = line[:-3].replace("D^", "").split("+")
             polynomial = [int(i) for i in polynomial]
             if polynomial[0] > max_deg:
                 break
 
             for term in polynomial:
-                bit_vect[int(term)] = "1"
-            print "".join(bit_vect)
+                bit_vect[term] = "1"
+            print "".join(bit_vect[::-1])
