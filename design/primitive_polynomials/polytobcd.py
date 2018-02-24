4ad1c9c394a8abb5a94c6f869187f25305d0ef16
sabbirahm3d <sabbir0ahmed0@gmail.com>
2018-02-24 15:40:39 -0500

    modularized process for generating list of primitive polynomials for n degree

--- design/primitive_polynomials/polytobcd.py
+++ design/primitive_polynomials/polytobcd.py
@@ -3,15 +3,26 @@
 
 if __name__ == "__main__":
 
-    max_deg = 15
-    with open("raw_primitive_polynomials.txt") as poly_file:
-        for line in poly_file:
+    from sys import argv
+
+    max_deg = int(argv[1])
+    with open(argv[2]) as poly_file:
+
+        poly_lines = [
+            line.replace("\n", "") for line in poly_file.read().split(" + 1")
+        ][:-1]
+
+        for line in poly_lines:
+
             bit_vect = ["1"] + ["0"] * max_deg
-            polynomial = line[:-3].replace("D^", "").split("+")
+            polynomial = line.replace("x^", "").split(" + ")
+            polynomial = ["1" if line == "x" else line for line in polynomial]
             polynomial = [int(i) for i in polynomial]
+
             if polynomial[0] > max_deg:
                 break
 
             for term in polynomial:
                 bit_vect[term] = "1"
+
             print "".join(bit_vect[::-1])
--- design/primitive_polynomials/primpoly/gen.sh
+++ design/primitive_polynomials/primpoly/gen.sh
@@ -13,8 +13,22 @@ DUMP="../primitive_polynomials_dump.txt"
 # raw primitive polynomials dump file
 RAW="../raw_primitive_polynomials.txt"
 
+FINALOUT="../primitivebcd15.txt"
+
+# clean up cache and old data
+make clean
+make build
+
+# loop through the degrees to generate their primitve polynomials
 for (( n = $MINDEG; n <= $MAXDEG; n++))
 do
+    echo "Generating primitive polynomials for n="$n;
     make run n=$n > ${DUMP};
     cat ${DUMP} | grep -E "^ (\+|x)" | sed 's/ x/x/g' | sed 's/ ^ /^/g' >> ${RAW};
 done
+
+# convert raw output to bit vectors
+echo -n "Converting polynomial strings to binary-coded decimals... "
+python ../polytobcd.py $MAXDEG $RAW > FINALOUT
+echo "Done"
+echo "Polynomials generated:" $(cat $FINALOUT | wc -l)
