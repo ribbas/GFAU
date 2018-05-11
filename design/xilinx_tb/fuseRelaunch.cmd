d5ec679a16ad45a64463abaabb73541e974b850a
sabbirahm3d <sabbir0ahmed0@gmail.com>
2018-05-11 16:17:11 -0400

    remove ISE files from Sabbir

--- design/xilinx_tb/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "secureip" -o "/home/sabbir/projects/GFAU/design/xilinx_tb/generator_tb_isim_beh.exe" -prj "/home/sabbir/projects/GFAU/design/xilinx_tb/generator_tb_beh.prj" "generator_tb" 
